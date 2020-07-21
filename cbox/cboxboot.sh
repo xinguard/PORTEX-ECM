b7a7e5936b69f9781d173b1ced584aac2643b282
Kae Hsu <kaehsu@gmail.com>
2020-07-22 02:10:14 +0800

    Update file path

--- cbox/cboxboot.sh
+++ cbox/cboxboot.sh
@@ -15,7 +15,7 @@ echo -e "green_blink\c" | sudo nc -q 1 -U /var/run/uds_led
 /home/portex/portex_ecm.d/ecm/ups_data.sh &>/dev/null 2>&1
 
 # Start UPS data upload
-/home/portex/portex_ecm.d/ups_http_upload.sh &>/dev/null 2>&1
+/home/portex/portex_ecm.d/ecm/ups_http_upload.sh &>/dev/null 2>&1
 
 echo -e "green_on\c" | sudo nc -q 1 -U /var/run/uds_led
 service bluetooth stop
